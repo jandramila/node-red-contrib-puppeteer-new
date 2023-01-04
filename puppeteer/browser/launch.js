f26e2db3e93d6c6bea6762cd8ba91395a264bc31
Javier Ramila <jandramila@gmail.com>
2023-01-04 01:21:38 -0300

    Fixes executable path config

--- puppeteer/browser/launch.js
+++ puppeteer/browser/launch.js
@@ -2,25 +2,25 @@ const puppeteer = require('puppeteer')
 
 
 module.exports = function (RED) {
-  function PuppeteerBrowserLaunch (config) {
+  function PuppeteerBrowserLaunch(config) {
     RED.nodes.createNode(this, config)
-    config.timeout = config.timeout == ""?0:config.timeout
-    config.slowMo = config.slowMo == ""?0:config.slowMo
-    config.debugport = config.debugport == ""?0:config.debugport 
+    config.timeout = config.timeout == "" ? 0 : config.timeout
+    config.slowMo = config.slowMo == "" ? 0 : config.slowMo
+    config.debugport = config.debugport == "" ? 0 : config.debugport
     // Retrieve the config node
     this.on('input', async function (msg) {
       try {
-        this.status({fill:"green",shape:"dot",text:"Launching..."});
-        msg.puppeteer = {browser:await puppeteer.launch( { timeout: config.timeout, slowMo: config.slowMo, executablePath: config.executablePath, headless: config.headless, debugport: config.debugport, devtools:config.devtools, defaultViewport: null, ignoreHTTPSErrors: true } )}
+        this.status({ fill: "green", shape: "dot", text: "Launching..." });
+        msg.puppeteer = { browser: await puppeteer.launch({ timeout: config.timeout, slowMo: config.slowMo, executablePath: config.executablePath, headless: config.headless, debugport: config.debugport, devtools: config.devtools, defaultViewport: null, ignoreHTTPSErrors: true }) }
         msg.puppeteer.page = (await msg.puppeteer.browser.pages())[0]
-        this.status({fill:"green",shape:"ring",text:"Launched"});
+        this.status({ fill: "green", shape: "ring", text: "Launched" });
         this.send(msg)
       } catch (e) {
-        this.status({fill:"red",shape:"ring",text:e});
+        this.status({ fill: "red", shape: "ring", text: e });
         this.error(e)
-      } 
+      }
     })
-    this.on('close', function() {
+    this.on('close', function () {
       this.status({});
     });
     oneditprepare: function oneditprepare() {
@@ -30,7 +30,7 @@ module.exports = function (RED) {
       $("#node-input-debugport").val(config.debugport)
       $("#node-input-devtools").val(config.devtools)
       $("#node-input-name").val(config.name)
-      $("#node-input-executable").val(config.executablePath)
+      $("#node-input-executablePath").val(config.executablePath)
     }
   }
   RED.nodes.registerType('puppeteer-browser-launch', PuppeteerBrowserLaunch)
